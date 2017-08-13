311c05445a61b1615dafba360de8202a53884b80
Lin Tao <lintaoo@hotmail.com>
2017-08-13 09:40:52 -0400

    Tempory change

--- Temp/Temp0725.py
+++ Temp/Temp0725.py
@@ -5,4 +5,4 @@ cols_names = ['IndexNo', 'Sequence', 'Operation', 'Source', 'Content', 'Thinking
 # data = pd.read_csv('Data/TempSamples2/T1_samples1.csv', names=cols_names, header=None)
 data = pd.read_csv('Data/TempSamples2/T1_samples2.csv', names=cols_names, header=None)
 # Content_Data = data['Content']
-import tkinter
+
