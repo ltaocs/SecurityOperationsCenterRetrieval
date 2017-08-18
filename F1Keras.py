8f6e4f13d951297e8ea37e009d301f0a0c44f340
Lin Tao <lintaoo@hotmail.com>
2017-08-18 11:34:53 -0400

    Calculate F1 score for Keras

---
+++ F1Keras.py
@@ -0,0 +1,68 @@
+# Cumstimed to calculate F1 score for Keras
+import pandas as pd
+from keras import backend as K
+from keras.layers import Dense
+from keras.models import Sequential
+from sklearn.model_selection import train_test_split
+
+
+# calculate F1 score
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        """Recall metric.
+
+        Only computes a batch-wise average of recall.
+
+        Computes the recall, a metric for multi-label classification of
+        how many relevant items are selected.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    def precision(y_true, y_pred):
+        """Precision metric.
+
+        Only computes a batch-wise average of precision.
+
+        Computes the precision, a metric for multi-label classification of
+        how many selected items are relevant.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2 * ((precision * recall) / (precision + recall))
+
+
+Data_Source = pd.read_csv('Data/TempSamples2/MyFile/NumberEndFile.csv')
+feature_cols = ['DSTPORT1', 'DSTPORT2', 'DSTPORT3', 'DSTPORT4', 'DSTPORT5', 'DSTPORT6', 'DSTPORT7', 'DSTPORT8',
+                'SRCPORT1', 'SRCPORT2', 'SRCPORT3', 'SRCPORT4', 'SRCPORT5', 'SRCPORT6', 'SRCPORT7', 'SRCPORT8',
+                'SRCIP1', 'SRCIP2', 'SRCIP3', 'SRCIP4', 'SRCIP5', 'SRCIP6', 'SRCIP7', 'SRCIP8',
+                'DSTIP1', 'DSTIP2', 'DSTIP3', 'DSTIP4', 'DSTIP5', 'DSTIP6', 'DSTIP7', 'DSTIP8']
+X = Data_Source[feature_cols].values
+Y = Data_Source['Result'].values
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=1)
+# create model
+model = Sequential()
+
+model.add(Dense(12, input_dim=32, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile model
+model.compile(loss='binary_crossentropy',
+              optimizer="adam",
+              metrics=[f1])
+
+model.fit(X_train, Y_train, epochs=100)
+
+# evaluate the model
+score = model.evaluate(X_test, Y_test, verbose=1)
+print(score)
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])
--- ProcessCSV.py
+++ ProcessCSV.py
@@ -3,7 +3,7 @@ import pandas as pd
 Number_End_File = pd.read_csv('Data/TempSamples2/MyFile/NumberEndFile.csv')
 
 # Number_End_File['SRCPORTSum']=Number_End_File
-# SRCPORTSum=Number_End_File['DSTPORT1':'SRCPORT1']
-Number_End_File['SRCPORTSum'] =
+SRCPORTSum = Number_End_File['DSTPORT1':'SRCPORT1']
+# Number_End_File['SRCPORTSum'] =
 
 print(SRCPORTSum.head())
